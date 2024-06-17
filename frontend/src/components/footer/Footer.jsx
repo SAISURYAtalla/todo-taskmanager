ec118412c7dcb44197fd7aef308923df7938658e
SAISURYAtalla <tallasai@iitbhilai.ac.in>
2024-06-17 23:26:58 +0530

    github hosted

---
+++ frontend/src/components/footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Footer.css";
+const Footer = () => {
+  return (
+    <div className="container-fluid p-3 d-flex justify-content-center align-items-center  footer">
+      <h4>TODO TASK MANAGER</h4> &nbsp; <p className="m-0">&copy;SAISURYATALLA</p>
+    </div>
+  );
+};
+
+export default Footer;
